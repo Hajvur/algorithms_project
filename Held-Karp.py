f7393a0f69291c25eddad9f552706cbf0e6797f2
Hajvur <IgorPyrczak@outlook.com>
2024-05-31 18:09:14 +0200

    add held-karp/minor fixes

---
+++ Held-Karp.py
@@ -0,0 +1,63 @@
+from itertools import combinations
+from scipy.spatial import distance_matrix
+from visualisation import attraction_list
+
+distance_matrix = distance_matrix(attraction_list, attraction_list)
+
+def held_karp(distance_matrix):
+    
+    n = len(distance_matrix)
+    
+    G = {}
+    
+    for k in range(1,n):
+        G[(1 << k,k)] = (distance_matrix[0][k],0)
+    
+    print(f"G = {G}")
+    for s in range(2,n):
+        for S in combinations(range(1,n),s):
+            print(f"S = {S}")
+            #ustawiamy bity z danej kombinacji jako sciezka, ktora przeszlismy np 1001 oznacza ze bylismy w punkcie 1 i w 4 (liczac od 1)
+            bity = 0
+            for bit in S:
+                #print(f"bit = {bin(bit)[2:]}")
+                bity |= 1 << bit
+                #print(f" bity = {bin(bity)[2:]}")
+            for k in S:
+                poprzednia_permutacja = bity & ~ (1<<k)
+                
+                result = []
+                for m in S:
+                    if m == 0 or m ==k:#jeżeli m bedzie "dworcem głownym" albo po przekatna macierzy to skipuje bo byloby 0
+                        continue
+                    result.append((G[(poprzednia_permutacja,m)][0] + distance_matrix[m][k],m))
+                G[(bity,k)] = min(result)
+
+                
+    bity = (2**n -1) -1
+    
+    result = []
+    for k in range(1, n):
+        result.append((G[(bity, k)][0] + distance_matrix[k][0], k))
+        print(f"curr res = {result}")
+    min_dist, parent = min(result)
+    
+    path = []
+    
+    for j in range(n-1):
+        path.append(parent)
+        new_bits = bity & ~(1 << parent)
+        holder, parent = G[(bity, parent)]
+        bity = new_bits
+    
+    path.append(0)
+    
+    print(f"sciezka = {list(reversed(path))}")
+    print(f"min dist  = {min_dist}")
+                
+
+                
+            
+            
+print(f"matrix = {distance_matrix}")
+held_karp(distance_matrix)
--- visualisation.py
+++ visualisation.py
@@ -1,4 +1,3 @@
-import enum
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -6,7 +5,10 @@ import numpy as np
 from matplotlib import image as mpimg
 from scipy.spatial import distance_matrix
 
+from naiwny import naive
+
 attraction_names = [
+    "dworzec główny",
     "hydropolis",
     "hala stulecia",
     "katedra",
@@ -15,11 +17,11 @@ attraction_names = [
     "rynek",
     "skytower",
     "opera",
-    "dworzec główny",
 ]
 #          wspolrzedne  x    y   jako x,y pikseli obrazu
 attraction_list = np.array(
     [
+        [309.3, 417.4],  # dworzec głowy
         [599.2, 339.2],  # hydropolis
         [782.4, 274.0],  # hala stulecia
         [421.3, 133.7],  # katedra
@@ -27,24 +29,22 @@ attraction_list = np.array(
         [348.4, 161.3],  # hala targowa
         [244.1, 216.5],  # rynek
         [109.1, 502.5],  # skytower
-        [253.3, 303.9],  # opera
-        [309.3, 417.4],  # dworzec głowy
-    ]
+        [253.3, 303.9],  # opera    
+        ]
 )
 
+
 def plots(attraction_list, attraction_names):
-    
     image = mpimg.imread("wroclaw.png")
     plt.imshow(image)
     plt.show()
     solution = np.arange(attraction_list.shape[0])
     plt.imshow(image)
 
-
     plt.scatter(attraction_list[:, 0], attraction_list[:, 1])
 
     for i in range(len(attraction_list)):
-        for j in range(i + 1, len(attraction_list)):
+        for j in range(i, len(attraction_list)):
             plt.plot(
                 [attraction_list[i, 0], attraction_list[j, 0]],
                 [attraction_list[i, 1], attraction_list[j, 1]],
@@ -63,17 +63,21 @@ def plots(attraction_list, attraction_names):
 
     plt.show()
 
+
 def graf(attraction_list, attraction_names):
     from scipy.spatial import distance_matrix
+
     distance_matrix = distance_matrix(attraction_list, attraction_list)
+    print(distance_matrix)
     G = nx.from_numpy_array(distance_matrix)
     mapping = {i: name for i, name in enumerate(attraction_names)}
     G = nx.relabel_nodes(G, mapping)
     return G
 
-def draw_graph(G):
-    pos = nx.spring_layout(G)  # pozycje dla wszystkich wierzchołków
 
+def draw_graph(G):
+    pos = nx.spring_layout(G)  
+    """
     nx.draw_networkx_nodes(G, pos, node_size=700)
 
     nx.draw_networkx_edges(G, pos)
@@ -82,12 +86,25 @@ def draw_graph(G):
 
     edge_labels = nx.get_edge_attributes(G, "weight")
     nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=6)
-
+    pos = nx.spring_layout(G)
+    """
+    edge_labels = nx.get_edge_attributes(G, "weight")
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=7)
+    nx.draw(
+        G,
+        pos,
+        with_labels=True,
+        node_color="skyblue",
+        edge_color="black",
+        node_size=500,
+        font_size=10,
+    )
     plt.show()
 
-if __name__=="__main__":
-    plots(attraction_list,attraction_names)
-    G = graf(attraction_list,attraction_names)
+
+if __name__ == "__main__":
+    plots(attraction_list, attraction_names)
+    G = graf(attraction_list, attraction_names)
     print(G)
     draw_graph(G)
-    
+    naive(G)
